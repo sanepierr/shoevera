1cf38c9d9092374fa0c5e023befee0aa2bd310b6
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2026-02-16 23:05:04 +0300

    updated social media links

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ export const WHATSAPP_NUMBER = '256705145159'
 export const WHATSAPP_MESSAGE = 'Hi, I want to order Shoevera'
 export const PHONE = '+256 705 145 159'
 export const EMAIL = 'hello@shoevera.com'
-export const INSTAGRAM_URL = 'https://instagram.com/shoevera'
-export const TIKTOK_URL = 'https://tiktok.com/@shoevera'
-export const FACEBOOK_URL = 'https://facebook.com/shoevera'
+export const INSTAGRAM_URL = 'https://www.instagram.com/shoevera.ug?igsh=YmYzNzlha2dsNXZp'
+export const TIKTOK_URL = 'https://www.tiktok.com/@shoevera_ug?lang=en-GB'
+export const FACEBOOK_URL = 'https://www.facebook.com/people/Shoeveraug/100069983687798/'
 
 export function getWhatsAppUrl(text = WHATSAPP_MESSAGE) {
   return `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(text)}`
